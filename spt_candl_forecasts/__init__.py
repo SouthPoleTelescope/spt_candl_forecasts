9c2206745f2ade5fd248f095acc4abe9e8810a17
Aline <aline.vitrier@iap.fr>
2025-11-20 14:51:45 +0100

    make pip install available

---
+++ spt_candl_forecasts/__init__.py
@@ -0,0 +1,5 @@
+"""Mock data for candl, the differentiable likelihood for CMB analysis"""
+
+__version__ = "1.0.0"
+__description__ = "Official SPT mock likelihoods for forecasts with candl."
+from .spt_candl_forecasts import *
